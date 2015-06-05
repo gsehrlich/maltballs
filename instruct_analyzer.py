57d7c6fcb91b3483180d7b2089f273adc0db6366
Gabriel Ehrlich <gehrlich@stanford.edu>
2015-06-05 14:58:07 -0700

    "data taken"

--- instruct_analyzer.py
+++ instruct_analyzer.py
@@ -9,14 +9,14 @@ span = {30: 20, # at 30 hz bandwidth, span is 20 hz ######EDITED 20 TO 40
 N_data_points = 101
 na_gpib_addr = 'gpib0::17::instr' # network analyzer
 volt_source_gpib_addr = "gpib0::5::instr" # voltage source for DC bias
-DC_on = 4 # volts
-DC_off = 0 # volts
-keep_peak_at_dbm = -39
+DC_biases = [8, 4] # volts
+DC_off = 0
+keep_peak_at_dbm = -40
 measurement_wait = {30: 0.2342/2*N_data_points + 1,
                     10: 0.9422/2*N_data_points + 1,
                     100: 0.1176/2*N_data_points + 1}
 time_fmt = "%04d-%02d-%02d %02d.%02d.%02d"
-direc = "data/run2/"
+direc = "data/run3/"
 
 # Passed as arg to time.sleep inside run_loop.py
 dt = 1 # sec; measurement itself takes long enough
@@ -26,16 +26,25 @@ def now():
 
 def get_and_center_argmax():
     volt_source.write('sour:volt %.2f' % DC_on)
-    na.write('bw 100')
+    na.write('bw 100') ######EDITED 100 TO 500
     na.write('sing')
     time.sleep(measurement_wait[100])
     na.write('seam max')
     na.write('mkrcent')
     return float(na.query('cent?'))
 
-def find_10hz_center(center30hz, center10hz):
-    Delta = center30hz - center10hz
-    return center10hz + Delta*(measurement_wait[10]/2)/(2*measurement_wait[30])
+def find_centers(n, dt):
+    # Estimate how fast the peak is moving, then return a list of n next centers
+
+    # Get two measurements at 100 hz
+    center1 = get_and_center_argmax()
+    center2 = get_and_center_argmax()
+    dt_100hz = measurement_wait[100]
+    peak_v = (center2 - center1)/dt_100hz
+
+    # Interpolate n next measurements
+    begin_center = center2 + 0.5*dt_100hz*peak_v
+    return tuple([begin_center + peak_v*(i + 0.5)*dt for i in xrange(n)])
 
 def autoscale():
     na.write("chan1")
@@ -57,7 +66,7 @@ def get_gain_phase():
     phase = np.array([float(g) for g in na.query_ascii_values('outpdtrc?')[::2]])
     return gain, phase
 
-def take_measurement(if_bw, freq_arr, DC_is_on, DC_bias):
+def take_measurement(if_bw, freq_arr, DC_bias):
     # set span
     na.write('span %.2f' % span[if_bw])
 
@@ -77,7 +86,7 @@ def take_measurement(if_bw, freq_arr, DC_is_on, DC_bias):
 
     # get gain + phase and write to file
     gain, phase = get_gain_phase()
-    filename = direc + "%dhz_%s/" % (if_bw, "osc" if DC_is_on else "ref")
+    filename = direc + "%dhz_%s/" % (if_bw, "%d_V" % DC_bias)
     filename += time_fmt % time_started
     header_lines = ["Time finished:",
                     time_fmt % time_finished,
@@ -113,12 +122,6 @@ volt_source.write('sour:volt %.2f' % DC_on)
 # autoscale so display is readable
 autoscale()
 
-# find peak freq and get new frequency array
-# autoscale again beacuse peak might have gone off the display
-center30hz = get_and_center_argmax()
-freq_arr = get_freq_arr()
-autoscale()
-
 # find peak val, then scale input voltage to keep const
 na.write('seam max')
 max_gain = float(na.query_ascii_values('outpmkr?')[0])
@@ -126,25 +129,33 @@ max_gain = float(na.query_ascii_values('outpmkr?')[0])
 new_source_dbm = keep_peak_at_dbm - max_gain
 na.write('powe %.6f' % new_source_dbm)
 
+# find centers for the next n measurements
+# autoscale again beacuse peak might have gone off the display
+centers = find_centers(len(DC_biases), measurement_wait[30])
+autoscale()
+
 # get oscillator measurement at 30 hz
-take_measurement(30, freq_arr, True, DC_on)
+for center, DC_bias in zip(centers, DC_biases):
+    na.write('cent %.7f' % center)
+    freq_arr = get_freq_arr()
+    volt_source.write('sour:volt %.2f' % DC_bias)
+    take_measurement(30, freq_arr, DC_bias)
+    volt_source.write('sour:volt %.2f' % DC_off)
+    take_measurement(30, freq_arr, DC_off)
 
-# get 30 hz reference measurement
-take_measurement(30, freq_arr, False, DC_off)
-
-# turn back on output for quick measurement,
-# find peak freq, and get new frequency array
-center10hz = get_and_center_argmax()
-
-# interpolate a good center value
-na.write('cent %.2f' % find_10hz_center(center30hz, center10hz))
-freq_arr = get_freq_arr()
+# find centers for the next n measurements
+# autoscale again beacuse peak might have gone off the display
+centers = find_centers(len(DC_biases), measurement_wait[10])
+autoscale()
 
 # get oscillator measurement at 10 hz
-take_measurement(10, freq_arr, True, DC_on)
-
-# get 10 hz reference measurement
-take_measurement(10, freq_arr, False, DC_off)
+for center, DC_bias in zip(centers, DC_biases):
+    na.write('cent %.7f' % center)
+    freq_arr = get_freq_arr()
+    volt_source.write('sour:volt %.2f' % DC_bias)
+    take_measurement(10, freq_arr, DC_bias)
+    volt_source.write('sour:volt %.2f' % DC_off)
+    take_measurement(10, freq_arr, DC_off)
 
 # set bw back to 100 hz, turn DC bias back on, center, and run continuously
 center_peak() # this does a bunch of stuff
